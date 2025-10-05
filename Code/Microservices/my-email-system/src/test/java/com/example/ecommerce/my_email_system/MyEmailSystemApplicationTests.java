d87698ab6c127c93ecaf286833ad88f703d49167
Mohamed Amine Kamel <m.kamel@tu-braunschweig.de>
2025-10-05 11:08:48 +0100

    update Project details

---
+++ Code/Microservices/my-email-system/src/test/java/com/example/ecommerce/my_email_system/MyEmailSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ecommerce.my_email_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyEmailSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
