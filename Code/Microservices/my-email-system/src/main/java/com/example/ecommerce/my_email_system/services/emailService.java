1eddc15f1b15ff3838fc6c4fe78492f62b4a2f13
Mohamed Amine Kamel <m.kamel@tu-braunschweig.de>
2025-11-10 17:55:04 +0100

    mailService implementation (basic)

--- Code/Microservices/my-email-system/src/main/java/com/example/ecommerce/my_email_system/services/emailService.java
+++ Code/Microservices/my-email-system/src/main/java/com/example/ecommerce/my_email_system/services/emailService.java
@@ -1,12 +1,35 @@
 package com.example.ecommerce.my_email_system.services;
 
-import com.example.ecommerce.my_email_system.model.basicEmail;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.stereotype.Service;
 
+import com.example.ecommerce.my_email_system.model.BasicEmail;
+
+import jakarta.mail.MessagingException;
+import jakarta.mail.internet.MimeMessage;
+
+@Service
 public class emailService {
 
-    public static void sendBasicEmail(basicEmail email) {
-        return;
+    @Autowired
+    private JavaMailSender mailSender;
+
+    public void sendMail(BasicEmail mail) throws MessagingException {
+        MimeMessage mimeMessage = mailSender.createMimeMessage();
+        MimeMessageHelper msg = new MimeMessageHelper(mimeMessage, true, "UTF-8");
+        msg.setTo(mail.getTo());
+        if (mail.getSubject() != null)
+            msg.setSubject(mail.getSubject());
+        else
+            msg.setSubject("no Subject");
+        msg.setText(mail.getHTML(), true);
+
+        mailSender.send(msg.getMimeMessage());
     }
+    // DIFFERENT IMPLEMENTATIONS OF sendMail() MIGHT BE NEEDED IN THE FUTURE
+
 }
 // requires : documentation of :
 // JavaMailSender
